a706eea57cf5743e41b39c43b251e8af9742cb0f
unknown <g00398265@gmit.ie>
2021-03-31 12:26:47 +0100

    Histograms working. General Histogram, and Hitograms separated by type of Flower

--- analysis.py
+++ analysis.py
@@ -18,6 +18,8 @@
 
 import csv
 import pandas as pd
+import matplotlib.pyplot as plt 
+import numpy as np
 
 filename='IrisData.csv'
 
@@ -71,6 +73,10 @@ dictLists={'Sepal Length':pd.Series(sepalLength), 'Sepal Width':pd.Series(sepalW
 df=pd.DataFrame(dictLists)                                          
 analysisPara=df.describe()                  #analysis of parameters (count, mean, stf, min, max and percentiles)
 
+
+#TRY TO SORT DECIMAL PLACES
+
+
 #Writing the info into the txt file:
 textFile='VariablesSummary.txt'
 
@@ -79,3 +85,87 @@ def writeFile ():
         tf.write(str(analysisPara))
 
 writeFile()
+
+
+#Histogram for each variable 
+
+
+#HISTOGRAMS
+#SEPAL LENGTH
+setosaSL=sepalLength[:50]
+versicolorSL=sepalLength[50:100]
+virginicaSL=sepalLength[100:]
+
+#bins1=np.linspace(4,9,100)    #[REF]: https://stackoverflow.com/questions/6871201/plot-two-histograms-on-single-chart-with-matplotlib
+
+#SEPAL WIDTH
+setosaSW=sepalWidth[:50]
+versicolorSW=sepalWidth[50:100]
+virginicaSW=sepalWidth[100:]
+
+#PETAL LENTGH
+setosaPL=petalLength[:50]
+versicolorPL=petalLength[50:100]
+virginicaPL=petalLength[100:]
+
+#PETAL WIDTH
+setosaPW=petalWidth[:50]
+versicolorPW=petalWidth[50:100]
+virginicaPW=petalWidth[100:]
+
+#
+sepalLengthHist=[setosaSL, versicolorSL, virginicaSL]
+sepalWidthHist=[setosaSW, versicolorSW, virginicaSW]
+petalLengthHist=[setosaPL, versicolorPL, virginicaPL]
+petalWidthHist=[setosaPW, versicolorPW, virginicaPW]
+
+def generalHist():
+    bins=15
+    fig, ((ax0, ax1), (ax2, ax3)) = plt.subplots(nrows=2, ncols=2)
+
+    ax0.hist(sepalLength, bins, density=True, histtype='bar')
+    ax0.set_title('Sepal Length')
+
+    ax1.hist(sepalWidth, bins, density=True, histtype='bar', stacked=True)
+    ax1.set_title('Sepal Width')
+
+    ax2.hist(petalLength, bins, histtype='step', stacked=True, fill=False)
+    ax2.set_title('Petal Length')
+    
+    ax3.hist(petalWidth, bins, histtype='bar')
+    ax3.set_title('Petal Width')
+
+    fig.tight_layout()
+    plt.savefig('general Hist.png')
+
+
+
+
+def histFlower():
+    bins=15
+    colors = ['red', 'tan', 'lime']
+    colours=['red','blue','green']
+    labels=['Setosa','Versicolor','Virginica']
+
+    fig, ((ax0, ax1), (ax2, ax3)) = plt.subplots(nrows=2, ncols=2)
+
+    ax0.hist(sepalLengthHist, bins, density=True, histtype='bar', color=colours, label=labels)
+    ax0.legend(prop={'size': 10})
+    ax0.set_title('Sepal Length')
+
+    ax1.hist(sepalWidthHist, bins, density=True, histtype='bar', color=colours, label=labels, stacked=True)
+    ax1.set_title('Sepal Width')
+
+    ax2.hist(petalLengthHist, bins, histtype='step', color=colours, label=labels, stacked=True, fill=False)
+    ax2.set_title('Petal Length')
+    # Make a multiple-histogram of data-sets with different length.
+    ax3.hist(petalWidthHist, bins, histtype='bar', color=colours, label=labels)
+    ax3.set_title('Petal Width')
+
+    fig.tight_layout()
+    plt.savefig('4plots.png')
+
+generalHist()
+histFlower()
+
+

2916c2ea25ef2f50efd3d251a095c0c91c4d2981
unknown <g00398265@gmit.ie>
2021-04-05 13:09:54 +0100

    Update of Scatter plots

--- analysis.py
+++ analysis.py
@@ -76,7 +76,7 @@ setosaPW=petalWidth[:50]
 versicolorPW=petalWidth[50:100]
 virginicaPW=petalWidth[100:]
 
-#Combining Lists to be able to identiy 
+#Combining Lists to be able to identiy data into plots
 sepalLengthHist=[setosaSL, versicolorSL, virginicaSL]
 sepalWidthHist=[setosaSW, versicolorSW, virginicaSW]
 petalLengthHist=[setosaPL, versicolorPL, virginicaPL]
@@ -131,33 +131,56 @@ def histFlower():
 #SCATTER:
 def scatterFlowers ():
     
-    fig, (ax0, ax1, ax2) = plt.subplots(nrows=1, ncols=3)
+    fig, ((ax0, ax1, ax2),(ax3, ax4, ax5)) = plt.subplots(nrows=2, ncols=3)
 
     ax0.scatter(sepalLength[:50], setosaSW, color='goldenrod', label='Setosa')      #[REF] data colors: https://matplotlib.org/stable/gallery/color/named_colors.html
     ax0.scatter(sepalLength[50:100], versicolorSW, color='salmon', label='Versicolor')
     ax0.scatter(sepalLength[100:], virginicaSW, color='cornflowerblue', label='Virginica')
-    ax0.set_xlabel('Sepal Length')                                                  #[REF] ax1.set_ylabel :https://stackoverflow.com/questions/6963035/pyplot-axes-labels-for-subplots
-    ax0.set_ylabel('Sepal Width')
+    ax0.set_xlabel('Sepal Length (cm)')                                                  #[REF] ax1.set_ylabel :https://stackoverflow.com/questions/6963035/pyplot-axes-labels-for-subplots
+    ax0.set_ylabel('Sepal Width (cm)')
     ax0.set_facecolor('ivory')                                                       #[REF] ax1.set_facecolor: https://stackoverflow.com/questions/14088687/how-to-change-plot-background-color
     ax0.legend()                                                                    #[REF] ax1.legend() https://stackoverflow.com/questions/52056261/how-to-set-label-for-each-subplot-in-a-plot-in-matplotlib
     
     ax1.scatter(sepalLength[:50], setosaPL, color='goldenrod', label='Setosa')
     ax1.scatter(sepalLength[50:100], versicolorPL, color='salmon', label='Versicolor')
     ax1.scatter(sepalLength[100:], virginicaPL, color='cornflowerblue', label='Virginica')
-    ax1.set_xlabel('Sepal Length')
-    ax1.set_ylabel('Petal Length')
+    ax1.set_xlabel('Sepal Length (cm)')
+    ax1.set_ylabel('Petal Length (cm)')
     ax1.set_facecolor('ivory')
     ax1.legend()
 
     ax2.scatter(sepalLength[:50], setosaPW, color='goldenrod', label='Setosa')
     ax2.scatter(sepalLength[50:100], versicolorPW, color='salmon', label='Versicolor')
     ax2.scatter(sepalLength[100:], virginicaPW, color='cornflowerblue', label='Virginica')
-    ax2.set_xlabel('Sepal Length')
-    ax2.set_ylabel('Petal Length')
+    ax2.set_xlabel('Sepal Length (cm)')
+    ax2.set_ylabel('Petal Length (cm)')
     ax2.set_facecolor('ivory')
     ax2.legend()
+
+    ax3.scatter(sepalWidth[:50], setosaSL, color='goldenrod', label='Setosa')      #[REF] data colors: https://matplotlib.org/stable/gallery/color/named_colors.html
+    ax3.scatter(sepalWidth[50:100], versicolorSL, color='salmon', label='Versicolor')
+    ax3.scatter(sepalWidth[100:], virginicaSL, color='cornflowerblue', label='Virginica')
+    ax3.set_xlabel('Sepal Width (cm)')                                                  #[REF] ax1.set_ylabel :https://stackoverflow.com/questions/6963035/pyplot-axes-labels-for-subplots
+    ax3.set_ylabel('Setal Length (cm)')
+    ax3.set_facecolor('ivory')                                                       #[REF] ax1.set_facecolor: https://stackoverflow.com/questions/14088687/how-to-change-plot-background-color
+    ax3.legend()                                                                    #[REF] ax1.legend() https://stackoverflow.com/questions/52056261/how-to-set-label-for-each-subplot-in-a-plot-in-matplotlib
+    
+    ax4.scatter(sepalWidth[:50], setosaPL, color='goldenrod', label='Setosa')
+    ax4.scatter(sepalWidth[50:100], versicolorPL, color='salmon', label='Versicolor')
+    ax4.scatter(sepalWidth[100:], virginicaPL, color='cornflowerblue', label='Virginica')
+    ax4.set_xlabel('Sepal Width (cm)')
+    ax4.set_ylabel('Petal Length (cm)')
+    ax4.set_facecolor('ivory')
+    ax4.legend()
+
+    ax5.scatter(sepalWidth[:50], setosaPW, color='goldenrod', label='Setosa')
+    ax5.scatter(sepalWidth[50:100], versicolorPW, color='salmon', label='Versicolor')
+    ax5.scatter(sepalWidth[100:], virginicaPW, color='cornflowerblue', label='Virginica')
+    ax5.set_xlabel('Sepal Width (cm)')
+    ax5.set_ylabel('Petal Width (cm)')
+    ax5.set_facecolor('ivory')
+    ax5.legend()
 
     plt.show()
 
 scatterFlowers()
-#ADD UNITS TO GRAPHS
